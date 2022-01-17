0d2e21fb29befcb7ae7cfad5eaeec0c5832c36a4
C1 <yarievakamila@gmail.com>
2022-01-17 16:31:03 +0300

    add selenoid support

--- src/test/java/yarieva/TestBase.java
+++ src/test/java/yarieva/TestBase.java
@@ -3,7 +3,9 @@ package yarieva;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import yarieva.pages.RegistrationsPage;
 
 public class TestBase {
@@ -14,6 +16,16 @@ public class TestBase {
         Configuration.browserSize = "2048x1153";
         Configuration.baseUrl = "https://demoqa.com";
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
     }
 
-}
+    }
+
+
