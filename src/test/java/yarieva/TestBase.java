9ed3f5a4895dc6b90d70d263bf1ae5136a1a54a4
C1 <yarievakamila@gmail.com>
2022-01-17 16:12:38 +0300

    add addListener

--- src/test/java/yarieva/TestBase.java
+++ src/test/java/yarieva/TestBase.java
@@ -1,6 +1,8 @@
 package yarieva;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import yarieva.pages.RegistrationsPage;
 
@@ -11,6 +13,7 @@ public class TestBase {
     static void beforeAll() {
         Configuration.browserSize = "2048x1153";
         Configuration.baseUrl = "https://demoqa.com";
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
 }
